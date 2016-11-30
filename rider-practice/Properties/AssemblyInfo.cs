45cb347bd6925acd4545917d886ee0b4f1e0acd6
Michael Hedgpeth <Michael Hedgpeth>
2016-11-29 23:04:57 -0600

    Initial commit

---
+++ rider-practice/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("rider_practice")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("rider_practice")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7DA9A9B1-C806-41DC-B5E2-BD78BF904921")]
---
+++ rider-practice/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace rider_practice
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1() 
+        {
+            Assert.True(true);
+        }
+    }
+}
